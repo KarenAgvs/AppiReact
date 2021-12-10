6a6b255e4cf08c552c87a20deea40a4477ff2cf8
Karen Agudelo <kdagudelo70@misena.edu.co>
2021-12-10 15:23:51 +0100

    Fix an error

--- src/Components/UI/SearchBar/SearchBar.jsx
+++ src/Components/UI/SearchBar/SearchBar.jsx
@@ -7,13 +7,13 @@ export const SearchBar=({data, event})=>{
             <div className="ContBar">
                 <h1 className="Title">Rick And Morty</h1>
                 <select name="option" id="select" onChange={event}>
-                    <option key="one" value="one">Select a character</option>
-                    <option key="all" value="all">Select all characters</option>
+                    <option key="one" value="one">Characters</option>
+                    <option key="all" value="all">All characters</option>
                     {
                         data.map(element => (
 
-                         <option key={element.id} value={element.name}>{element.name}</option>
-                         ))
+                            <option key={element.id} value={element.name}>{element.name}</option>    
+                        ))
                     }
                 </select>
             </div>
