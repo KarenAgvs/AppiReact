3f0836883792115209cb26a512383d3e96644d8f
Karen Agudelo <kdagudelo70@misena.edu.co>
2021-12-10 15:07:47 +0100

    Finshed the main

--- src/Components/Layouts/Main/Main.jsx
+++ src/Components/Layouts/Main/Main.jsx
@@ -2,3 +2,60 @@
 import { Card } from "../../UI/Card/Card";
 import { SearchBar } from "../../UI/SearchBar/SearchBar";
 import React,{useEffect,useState} from "react";
+
+export const Main=()=>{
+    useEffect(()=>{
+        appiFetch('');
+    },[]);
+    const [datas,setData]=useState([{name:"Rick",image:rick}]);
+    const [datos,setDatos]=useState([{name:"Rick",image:rick}]);
+    const[name,setName]=useState([{id:1,name:"Rick",image:rick}]);
+
+    const URL='https://rickandmortyapi.com/api/character/?name=';
+
+    const appiFetch=(()=>{
+        fetch(URL)
+        .then(response=>response.jason())
+        .then(data=>{
+            setData(data.results)
+            setName(data.results)
+            setDatos([])
+        });
+    })
+    const sedEvent=((event)=>{
+        if(event.target.value === 'one'){
+            setDatos([])
+            console.log(event.target);
+        }   
+        if(event.target.value === 'all'){
+            setDatos(datas)
+        }
+        datas.map((element) =>{
+            if (event.target.value===element.name) {
+                setDatos([element])
+                
+            }
+            return '';
+        })
+        
+    })
+
+    return(
+        <>
+        <main>
+            <div className="BigContainer">
+                <Select key="select" data={name} event={sedEvent}></Select>
+            </div>
+            <section>
+                
+                <div className="totalcards">
+                    <div className="allcard">
+                        <Card key="card" data={datos}/>
+                    </div>
+                </div>
+            </section>
+        </main>
+        
+        </>
+    )
+}
